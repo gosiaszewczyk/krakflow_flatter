cf35d553188bd598b9d538a5efe7c17002f2a4f9
gosiaszewczyk <gosiaxszewczyk05@gmail.com>
2026-04-09 15:40:41 +0200

    lab4

---
+++ android/app/src/main/kotlin/com/uken/krakflow/krakflow/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.uken.krakflow.krakflow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,225 @@
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+
+  runApp(const MyApp());
+
+}
+
+
+
+class Task {
+
+  final String title;
+
+  final String deadline;
+
+  final bool done;
+
+  final String priority;
+
+
+
+  Task({
+
+    required this.title,
+
+    required this.deadline,
+
+    required this.done,
+
+    required this.priority,
+
+  });
+
+}
+
+
+
+class MyApp extends StatelessWidget {
+
+  const MyApp({super.key});
+
+
+
+  @override
+
+  Widget build(BuildContext context) {
+
+    final List<Task> tasks = [
+
+      Task(
+
+        title: "Przygotować prezentację",
+
+        deadline: "jutro",
+
+        done: true,
+
+        priority: "wysoki",
+
+      ),
+
+      Task(
+
+        title: "Oddać raport z laboratoriów",
+
+        deadline: "dzisiaj",
+
+        done: false,
+
+        priority: "wysoki",
+
+      ),
+
+      Task(
+
+        title: "Powtórzyć widgety Flutter",
+
+        deadline: "w piątek",
+
+        done: false,
+
+        priority: "średni",
+
+      ),
+
+      Task(
+
+        title: "Napisać notatki do kolokwium",
+
+        deadline: "w weekend",
+
+        done: false,
+
+        priority: "niski",
+
+      ),
+
+    ];
+
+
+
+    int completedCount = tasks.where((t) => t.done).length;
+
+
+
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+
+      title: 'KrakFlow',
+
+      home: Scaffold(
+
+        appBar: AppBar(
+
+          title: const Text("KrakFlow"),
+
+        ),
+
+        body: Padding(
+
+          padding: const EdgeInsets.all(16.0),
+
+          child: Column(
+
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+
+              Text(
+
+                "Masz dziś ${tasks.length} zadania",
+
+                style: const TextStyle(fontSize: 18),
+
+              ),
+
+              Text(
+
+                "Wykonano: $completedCount",
+
+                style: const TextStyle(fontSize: 16, color: Colors.grey),
+
+              ),
+
+              const SizedBox(height: 16),
+
+              const Text(
+
+                "Dzisiejsze zadania",
+
+                style: TextStyle(
+
+                  fontSize: 22,
+
+                  fontWeight: FontWeight.bold,
+
+                ),
+
+              ),
+
+              const SizedBox(height: 10),
+
+              Expanded(
+
+                child: ListView.builder(
+
+                  itemCount: tasks.length,
+
+                  itemBuilder: (context, index) {
+
+                    return TaskCard(task: tasks[index]);
+
+                  },
+
+                ),
+
+              ),
+
+            ],
+
+          ),
+
+        ),
+
+      ),
+
+    );
+
+  }
+
+}
+
+
+
+class TaskCard extends StatelessWidget {
+
+  final Task task;
+
+
+
+  const TaskCard({super.key, required this.task});
+
+
+
+  @override
+
+  Widget build(BuildContext context) {
+
+    return Card(
+
+        child: ListTile(
+
+            leading: Icon(
+
+              task.done ? Icons.check_circle : Icons.radio_button_unchecked,
+
+            ),
+
+            title: Text(task.title),
+
+            subtitle: Text("termin: ${task.deadline} | priorytet:
